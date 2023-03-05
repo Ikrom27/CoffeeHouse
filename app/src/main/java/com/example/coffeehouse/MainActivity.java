b85d8d93b393b62a3571bed23bc886d63a0818de
Ikrom27 <ikrom.hasanbaev.27@gmail.com>
2023-03-05 14:04:59 +0300

    Button handled added

--- app/src/main/java/com/example/coffeehouse/MainActivity.java
+++ app/src/main/java/com/example/coffeehouse/MainActivity.java
@@ -13,74 +13,22 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MyLogs";
-
-    protected void createToast(String text){
-        /*
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show(); */
-    }
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_welcome);
-
-        createToast("onCreate");
-        Log.i(TAG, "Приложение запущено");
-        Log.e(TAG, "Данные не найдены");
-    }
-
-    public void onClick(View view){
-        Intent intent = new Intent(this, SignInActivity.class);
-        startActivity(intent);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        createToast("onStart");
-        Log.d(TAG, "onStart");
-        Log.w(TAG, "Заканчивается память");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        createToast("onResume");
-        Log.d(TAG, "onResume");
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        createToast("onPause");
-        Log.d(TAG, "onPause");
-        Log.v(TAG, "приложение было свернуто уже много раз");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        createToast("onStop");
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        createToast("onDestroy");
-        Log.d(TAG, "onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        createToast("onRestart");
-        Log.d(TAG, "onRestart");
+        ImageButton bt_forward = findViewById(R.id.bt_wcscreen);
+        bt_forward.setOnClickListener(view -> {
+            Log.d(TAG, "Button click handled");
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/coffeehouse/SignInActivity.java
+++ app/src/main/java/com/example/coffeehouse/SignInActivity.java
@@ -2,10 +2,12 @@ package com.example.coffeehouse;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignInActivity extends AppCompatActivity {
+    private static final String TAG = "SignInActivity";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,12 +15,8 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     public void onClick(View view){
+        Log.d(TAG, "Button click handled");
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
     }
 }
